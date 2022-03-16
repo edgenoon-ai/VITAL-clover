da1b1dec483dfc645a0acf7001ffbfa42a4e3ab5
Bartosz Ptak <bartptak@gmail.com>
2022-03-16 14:12:40 +0100

    Update

--- docker/create_and_run.sh
+++ docker/create_and_run.sh
@@ -1,8 +1,8 @@
-docker build -t px4-clover -f Dockerfile . --no-cache
+docker build -t px4-sim -f Dockerfile . --no-cache
 
 mkdir $HOME/shared
 
 chmod +x xauth.sh
 ./xauth.sh
 
-docker run -it --gpus all --privileged --env="DISPLAY=:1" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw"     --env="XAUTHORITY=$XAUTH"     --volume="$XAUTH:$XAUTH"     --volume="$HOME/shared:/home/px4/share:rw"     --env="NVIDIA_VISIBLE_DEVICES=all"     --env="NVIDIA_DRIVER_CAPABILITIES=all"     --network=host --name=px4-clover px4-clover bash
+docker run -it --gpus all --privileged --env="DISPLAY=:1" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw"     --env="XAUTHORITY=$XAUTH"     --volume="$XAUTH:$XAUTH"     --volume="$HOME/shared:/home/px4/share:rw"     --env="NVIDIA_VISIBLE_DEVICES=all"     --env="NVIDIA_DRIVER_CAPABILITIES=all"     --network=host --name=px4_sim px4-sim bash
